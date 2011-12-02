fff63617bd7717091d4223c18413b8512f4cb22c
John Martin <j.marmar47@gmail.com>
2011-12-02 13:03:48 -0600

    fixed alot of stuff

--- Chatterbox/Chatterbox/src/me/jobud9andhammale/ChatterBox/main.java
+++ Chatterbox/Chatterbox/src/me/jobud9andhammale/ChatterBox/main.java
@@ -7,30 +7,36 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin {
-	public String PluginDirPath;
-	public File ConfigFile;
-    public server webserver = new server();
-	final ChatHandler playerListener = new ChatHandler(this);
-	public void toConsole(String msg, int type) {
-		Logger log = Logger.getLogger("Minecraft");
-		switch(type) {
-		case 1:log.info   ("[Wampom] "+msg); break;
-		case 2:log.warning("[Wampom] "+msg); break;
-		case 3:log.severe ("[Wampom] "+msg); break;
-		}
-	}
-	  
-	   	public void onEnable(){
-	   		this.PluginDirPath = this.getDataFolder().getAbsolutePath();
-	   		this.ConfigFile = new File(this.PluginDirPath + File.separator + "config.yml");
-	   		this.webserver = new server();
-	        PluginManager pm = this.getServer().getPluginManager();
-	        pm.registerEvent(Event.Type.PLAYER_CHAT, playerListener, Event.Priority.Normal, this);
-	        
-	        this.toConsole("Enabled without error!", 1);
-	    }
+    public String PluginDirPath;
+    public File ConfigFile;
+    public server webserver;
+    final ChatHandler playerListener = new ChatHandler(this);
 
-		public void onDisable() {
-			
-		}
-}	
+    public void toConsole(String msg, int type) {
+        Logger log = Logger.getLogger("Minecraft");
+        switch (type) {
+        case 1:
+            log.info("[Wampom] " + msg);
+            break;
+        case 2:
+            log.warning("[Wampom] " + msg);
+            break;
+        case 3:
+            log.severe("[Wampom] " + msg);
+            break;
+        }
+    }
+
+    public void onEnable() {
+        this.PluginDirPath = this.getDataFolder().getAbsolutePath()+File.separator;
+        this.ConfigFile = new File(this.PluginDirPath + File.separator + "config.yml");
+        this.webserver = new server(this);
+        PluginManager pm = this.getServer().getPluginManager();
+        pm.registerEvent(Event.Type.PLAYER_CHAT, playerListener,
+                Event.Priority.Normal, this);
+        this.toConsole("Enabled without error!", 1);
+    }
+
+    public void onDisable() {
+    }
+}
--- Chatterbox/Chatterbox/src/me/jobud9andhammale/ChatterBox/server.java
+++ Chatterbox/Chatterbox/src/me/jobud9andhammale/ChatterBox/server.java
@@ -2,62 +2,62 @@ package me.jobud9andhammale.ChatterBox;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 public class server {
-  public main plugin;
-  protected void start(main plugin) {
-	this.plugin = plugin;
-    ServerSocket s;
+    public main plugin;
 
-    try {
-      s = new ServerSocket(80);
-    } catch (Exception e) {
-      plugin.toConsole("Cannot bind to port! Port is in use!", 3);
-      return;
+    protected void start(main plugin) {
+        this.plugin = plugin;
+        ServerSocket s;
+        try {
+            s = new ServerSocket(80);
+        } catch (Exception e) {
+            plugin.toConsole("Cannot bind to port! Port is in use!", 3);
+            return;
+        }
+        for (;;) {
+            try {
+                Socket remote = s.accept();
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        remote.getInputStream()));
+                PrintWriter out = new PrintWriter(remote.getOutputStream());
+                String str = ".";
+                while (!str.equals(""))
+                    str = in.readLine();
+                out.println("HTTP/1.0 200 OK");
+                out.println("Content-Type: text/html");
+                out.println("Server: Chatterbox");
+                out.println("");
+                try {
+                    FileInputStream fstream = new FileInputStream(plugin.PluginDirPath +"html"+File.separator+"index.htm");
+                    DataInputStream input = new DataInputStream(fstream);
+                    BufferedReader br = new BufferedReader(
+                            new InputStreamReader(input));
+                    String strLine;
+                    while ((strLine = br.readLine()) != null) {
+                        out.println(strLine);
+                    }
+                    input.close();
+                } catch (Exception e) {
+                    out.println("Error: " + e.getMessage());
+                }
+                out.flush();
+                remote.close();
+            } catch (Exception e) {
+                plugin.toConsole("Error: " + e, 2);
+            }
+        }
     }
 
-    for (;;) {
-      try {
-        Socket remote = s.accept();
-        BufferedReader in = new BufferedReader(new InputStreamReader(
-            remote.getInputStream()));
-        PrintWriter out = new PrintWriter(remote.getOutputStream());
-        String str = ".";
-        while (!str.equals(""))
-          str = in.readLine();
-        out.println("HTTP/1.0 200 OK");
-        out.println("Content-Type: text/html");
-        out.println("Server: Chatterbox");
-        out.println("");
-        try{
-        	  FileInputStream fstream = new FileInputStream("index.htm");
-        	  DataInputStream input = new DataInputStream(fstream);
-        	  BufferedReader br = new BufferedReader(new InputStreamReader(input));
-        	  String strLine;
-        	  while ((strLine = br.readLine()) != null)   {
-        out.println (strLine);
-        	  }
-        	  input.close();
-        	    }catch (Exception e){
-        	  System.err.println("Error: " + e.getMessage());
-        	  }
-        out.flush();
-        remote.close();
-      } catch (Exception e) {
-        plugin.toConsole("Error: " + e, 2);
-      }
+    public server(main m) {
+
+        this.start(m);
+
     }
-  }
-  public static void main(String args[]) {
-    WebServer ws = new WebServer();
-    ws.start();
-  }
 }
