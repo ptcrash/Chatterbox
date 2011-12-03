3e517a1d20a4a4f0c028f098f84247ede7b7fd3c
John Martin <j.marmar47@gmail.com>
2011-12-02 22:29:11 -0600

    MORE updates

--- chatterbox/src/me/jobud9andhammale/ChatterBox/main.java
+++ chatterbox/src/me/jobud9andhammale/ChatterBox/main.java
@@ -3,6 +3,7 @@ package me.jobud9andhammale.ChatterBox;
 import java.io.File;
 import java.util.logging.Logger;
 import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,24 +11,26 @@ public class main extends JavaPlugin {
     public String PluginDirPath;
     public File ConfigFile;
     public configHandler config;
+    public PluginDescriptionFile pdfile;
     final ChatHandler playerListener = new ChatHandler(this);
 
     public void toConsole(String msg, int type) {
         Logger log = Logger.getLogger("Minecraft");
         switch (type) {
         case 1:
-            log.info("[Chatterbox] " + msg);
+            log.info("["+pdfile.getName()+"] " + msg);
             break;
         case 2:
-            log.warning("[Chatterbox] " + msg);
+            log.warning("["+pdfile.getName()+"] " + msg);
             break;
         case 3:
-            log.severe("[Chatterbox] " + msg);
+            log.severe("["+pdfile.getName()+"] " + msg);
             break;
         }
     }
 
     public void onEnable() {
+    	this.pdfile = this.getDescription();
         this.PluginDirPath = this.getDataFolder().getAbsolutePath()+File.separator;
         this.ConfigFile = new File(this.PluginDirPath + File.separator + "config.yml");
         this.config = new configHandler(this.ConfigFile);
